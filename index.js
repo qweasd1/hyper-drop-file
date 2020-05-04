757cfcbb35871cdbabe35a52931362045afdc854
Kdump <rootshellexp@gmail.com>
2020-05-05 05:35:14 +0800

    add win2posix function

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ SESSION_USER_DATA = "SESSION_USER_DATA"
 
 let _store
 
-const WHITESPACE_PATTERN = / /g
+const WHITESPACE_PATTERN = / |%20/g
+
+const DEFAULTS = {
+  DRIVE_PREFIX: '',
+  ENABLE_WIN2POSIX: false
+};
+
+const hyperDropFileConfig = {}
+
+function win2posix(path) {
+  tmpPath = new URL(path).pathname
+  drive = tmpPath.match(/^(\/[a-zA-Z])/)[0].toLowerCase()
+  tmpPath.replace(/^\/[a-zA-Z]\:/, drive)
+  return `${hyperDropFileConfig.DRIVE_PREFIX}${tmpPath.replace(/^\/[a-zA-Z]\:/, drive)}`
+}
 
 function escapeWhitepsaceInDirectory(path) {
   // only support linux and mac style now
@@ -32,19 +46,19 @@ function getCurrentWindow(){
 
 exports.onRendererWindow = (window) => {
   window.document.addEventListener('DOMContentLoaded', () => {
-
+    Object.assign(hyperDropFileConfig, DEFAULTS, config.getConfig().hyperDropFile);      
     const dropTargetElement = window.document.querySelector("#hyper") || window.document.querySelector(".hyper_main")
 
     dropTargetElement.addEventListener("dragover", (e) => {
       e.preventDefault()
       e.stopPropagation()
     })
-  //
+    window.a=hyperDropFileConfig
     dropTargetElement.addEventListener("drop", (e) => {
-      // e.preventDefault()
-      // e.stopPropagation()
       for (let file of e.dataTransfer.files) {
-        sendSessionData(null, escapeWhitepsaceInDirectory(file.path))
+        optimizePath = file.path
+        hyperDropFileConfig.ENABLE_WIN2POSIX && (optimizePath = win2posix(file.path))
+        sendSessionData(null, escapeWhitepsaceInDirectory(optimizePath))
         break
       }
 
